d49b620e07facb9088e16772f361b2d7b9c69679
Inesa Toroyan <toroyaninesa@gmail.com>
2021-10-01 17:28:18 +0400

    auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,3 +3,42 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 router.use(cors());
+
+// register
+
+
+router.post('/register', async (req,res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            name:req.body.name,
+            surname:req.body.surname,
+            username:req.body.username,
+            email:req.body.email,
+            password:hashedPass,   
+        });
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+//login
+
+router.post('/login', async (req, res ) => {
+    try{
+    const user = await User.findOne({username:req.body.username});
+    !user && res.status(400).json("Wrong creditentials");
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong creditentials");
+    const { password, ...others} = user._doc;
+    res.status(200).json(others)
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router
