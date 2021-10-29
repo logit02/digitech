61e7763fcc199ab64b9826eec0c814c2e36cfd9b
Inesa Toroyan <toroyaninesa@gmail.com>
2021-10-29 17:28:54 +0400

    index.js note form inescomp

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 app.use(express.json());
 const jwt = require("jsonwebtoken")
 const path = require('path')
-
+//from ines komp
 //Routes
 const postRoute = require('./routes/posts')
 const NewsRoute = require('./routes/news')
